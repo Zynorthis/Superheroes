85a67dd04213fa9f1b3f53dbb005d50b2eb455b9
Jacob Taylor <jacobtaylor727@outlook.com>
2019-01-09 21:52:10 -0600

    Removed the Details Page Test button from the nav bar. Removed a few columns from the table displaying all superheroes on the index page.

--- Superheroes/Models/Superhero.cs
+++ Superheroes/Models/Superhero.cs
@@ -10,11 +10,15 @@ namespace Superheroes.Models
     {
         [Key]
         public int ID { get; set; }
+        [Display(Name = "Name")]
         public string Name { get; set; }
         [Display(Name="Alter Ego")]
         public string alterEgo { get; set; }
+        [Display(Name = "Primary Ability")]
         public string primaryAbility { get; set; }
+        [Display(Name = "Secondary Ability")]
         public string secondaryAbility { get; set; }
+        [Display(Name = "Catchphrase")]
         public string catchphrase { get; set; }
     }
 }
